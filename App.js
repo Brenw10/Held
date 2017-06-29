ca98f29f9bf64a27d527d35eef1a0442caef9c99
Brendon Willian <bmota@its.jnj.com>
2017-06-29 16:45:59 -0300

    Updated scaffolding

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import LoginPage from 'held/app/pages/login/login';
+import LoginPage from 'held/app/pages/login';
 
 export default class App extends React.Component {
   render() {
