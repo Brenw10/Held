9e17352d69c4bddffa0931f91b05b312a94cd342
Brendon Willian <bmota@its.jnj.com>
2017-06-30 15:03:38 -0300

    Removed component (it is not a component), make navigation works (is showing an error)

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import LoginPage from 'held/app/pages/login';
+import HomePage from 'held/app/pages/home';
 
 import { Button } from 'react-native';
 import { TabNavigator } from 'react-navigation';
@@ -9,7 +10,10 @@ class App extends React.Component {
 }
 
 export default TabNavigator({
+  Login: {
+    screen: LoginPage
+  },
   Home: {
-    screen: LoginPage,
+    screen: HomePage,
   },
 });
--- app/pages/login.js
+++ app/pages/login.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Image, Text } from 'react-native';
+import * as firebase from 'firebase';
 
-import FacebookLogin from 'held/app/components/facebook/login';
+export default class LoginPage extends React.Component {
+  constructor() {
+    super();
 
-export default class App extends React.Component {
+    //Todo: Read it from parameter place
+    var config = {
+      apiKey: "AIzaSyCc-gCBjPIW-CcIAHxttdB9cHks7W_t1R0",
+      authDomain: "held-efdf8.firebaseapp.com",
+      databaseURL: "https://held-efdf8.firebaseio.com",
+      projectId: "held-efdf8",
+      storageBucket: "held-efdf8.appspot.com",
+      messagingSenderId: "828413601398"
+    };
+    firebase.initializeApp(config);
+  }
+
   static navigationOptions = {
     tabBarVisible: false
   };
 
+  async handleFacebookLogin() {
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('453060115055613', {
+      permissions: ['email', 'public_profile', 'user_friends'],
+    });
+    if (await type === 'success') {
+      this.handleHomePage(await this.getFirebaseAuth(await token));
+    }
+  }
+
+  getFirebaseAuth(token) {
+    const facebookCredential = firebase.auth.FacebookAuthProvider.credential(token);
+    return firebase.auth().signInWithCredential(facebookCredential);
+  }
+
+  handleHomePage(auth) {
+    this.props.navigation.navigate('Home');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <FacebookLogin />
+        <TouchableOpacity onPress={() => this.handleFacebookLogin()} style={styles.button}>
+          <Image source={require('held/assets/images/facebook-logo.png')} style={styles.logo} />
+          <Text style={styles.text}>Login</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -24,5 +59,18 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  button: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 60,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: '#FFF',
+  },
 });
